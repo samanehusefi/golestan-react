3f1bdf51c95918c67686ea1833f25c28e4bc8c6b
samanehyousefi <samanehyousefi64@gmail.com>
2026-06-30 13:08:44 +0330

    bese address

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 
+  base: "/golestan-react/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
