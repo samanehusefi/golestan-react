fccb149bf6053ebe455a546a54a23dbac7bd1e0d
samanehyousefi <samanehyousefi64@gmail.com>
2026-06-30 14:18:04 +0330

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import path from "node:path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
   base: "/golestan-react/",
   resolve: {
     alias: {
